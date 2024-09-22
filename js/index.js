a5afce923f3a3c18034ed351cc639e34e8babe35
tammykhan <tammykhan543@gmail.com>
2024-09-22 22:44:00 +0600

    history section done

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
 const calculateButton = document.getElementById("calculate");
 calculateButton.addEventListener('click', function(){
 
-  const income = parseFloat(document.getElementById("income").value);
+ const income = parseFloat(document.getElementById("income").value);
 const software = parseFloat(document.getElementById("software").value);
 const courses = parseFloat(document.getElementById("courses").value);
 const internet = parseFloat(document.getElementById("internet").value);
@@ -21,6 +21,19 @@ const internet = parseFloat(document.getElementById("internet").value);
 
   const result = document.getElementById("results");
   result.classList.remove("hidden");
+
+  const historyItem = document.createElement("div");
+  historyItem.className = 
+  " bg-white p-3 rounded-md border-1-2 border-indigo-500 "
+
+  historyItem.innerHTML = `
+    <p class="text-xs text-gray-500" >${new Date().toLocaleDateString()} </p>
+    <p class="text-xs text-gray-500" >Income: $${income.toFixed(2)}</p>
+    <p class="text-xs text-gray-500" >Expenses: $${totalExpenses.toFixed(2)}</p>
+    <p class="text-xs text-gray-500" >Balance: $${balance.toFixed(2)}</p>
+  `
+  const historyContainer = document.getElementById("history-list");
+  historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 
 });
 
@@ -48,6 +61,32 @@ const internet = parseFloat(document.getElementById("internet").value);
   const remainingElement = document.getElementById('remaining-balance');
   remainingElement.innerText = remainingBalance.toFixed(2);
 
-
-
 })
+
+
+// history tab functionality
+const historyTab = document.getElementById("history-tab");
+const assistantTab = document.getElementById("assistant-tab");
+
+historyTab.addEventListener("click", function(){
+
+  historyTab.classList.add(
+    'text-white',
+    'font-semibold',
+     'bg-gradient-to-r', 
+     'from-blue-500', 
+     'to-purple-600'
+  );
+  historyTab.classList.remove('text-gray-600');
+  assistantTab.classList.remove(
+    'text-white',
+    'font-semibold',
+     'bg-gradient-to-r', 
+     'from-blue-500', 
+     'to-purple-600'
+  );
+  assistantTab.classList.add('text-gray-600');
+  document.getElementById('expense-form').classList.add('hidden');
+  document.getElementById('history-section').classList.remove('hidden');
+
+});
